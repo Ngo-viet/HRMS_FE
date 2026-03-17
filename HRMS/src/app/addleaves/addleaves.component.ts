eb52ed36c67520f43c7eeef7106b3d50716f9bcc
vietnd <ngoviet9898@gmail.com>
2026-03-17 20:48:16 +0700

    init angular project

---
+++ HRMS/src/app/addleaves/addleaves.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HrmsService } from '../hrms.service';
+import { Leaves } from '../leaves';
+
+@Component({
+  selector: 'app-addleaves',
+  templateUrl: './addleaves.component.html',
+  styleUrls: ['./addleaves.component.css']
+})
+export class AddleavesComponent implements OnInit {
+
+  leaves=new Leaves();
+  not_available=false;
+  msg="";
+  constructor(private service: HrmsService,private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  addLeaves()
+  {
+    this.service.addLeavesFromRemote(this.leaves).subscribe(
+      data => {
+        console.log("Leaves Added Successfully ");
+        this.not_available=true;
+        this.msg="Employee Leaves Added Successfully"
+      },
+      error => {
+        console.log("exception occured");
+        this.msg="Employee is not present with given Id"
+
+      }
+    )
+  }
+}
---
+++ HRMS/src/app/leaves.ts
@@ -0,0 +1,11 @@
+export class Leaves 
+{
+    lid:number;
+    eid:number;
+    leaveResone:string;
+    fromDate:string;
+    toDate:string;
+    description:string;
+	email:string;
+    constructor() {}
+}
